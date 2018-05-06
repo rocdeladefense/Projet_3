ecb6332ba63f82da73c7734907b67f75a42e7042
rocdeladefense <remi.1.2@hotmail.fr>
2018-05-06 19:20:25 +0200

    finitions de la classe challenger POM normalement

--- Projet3/src/fr/plusoumoins/POMChallenger.java
+++ Projet3/src/fr/plusoumoins/POMChallenger.java
@@ -6,30 +6,31 @@ public class POMChallenger {
 	    public void init(int nbTours, int grandeurDuNb) {
 	    	Utile utile = new Utile();
 	    	int nbAleatoire = ((int)( Math.random()*( 10000)) + 1);
-	        int nbAleatoireAdapt[] = utile.transformationIntEnArrayInt(nbAleatoire);
 	        boolean victoire = false;
+	        boolean verificationNb = true;
 	        while (nbTours > 0 && victoire == false)
 	        {
 	                System.out.println("Quelle est votre proposition ? (Entrez un nombre à " + grandeurDuNb + " chiffres)");
 	                Scanner scb = new Scanner(System.in);
-	                int proposition = scb.nextInt();
-	                int propositionAdapt[] = utile.transformationIntEnArrayInt(proposition);
-	                if (grandeurDuNb == propositionAdapt.length)
+	                String proposition = scb.nextLine();
+	                verificationNb = utile.verificationNb(proposition, grandeurDuNb);
+	                if (verificationNb == true)
 	               	{
-	                	if (proposition == nbAleatoire)
+	                	int propositionInt = utile.transformationStringEnInt(proposition); 
+	                	if (propositionInt == nbAleatoire)
 	                	{
 	                		System.out.println("====");
 	                		victoire = true;
 	                	}
 	                	else
 	                	{
-	                		utile.comparaisonPlusOuMoins(nbAleatoireAdapt, propositionAdapt, grandeurDuNb);
+	                		utile.comparaisonPlusOuMoins(nbAleatoire, propositionInt, grandeurDuNb);
 	                		nbTours--;	                
 	                	}
 	                }
 	                else 
 	                {
-	                	System.out.println("Vous avez saisi le mauvais nombre de chiffre, veuillez recommencer svp");
+	                	System.out.println("Vous avez saisi le mauvais nombre de chiffre, ou avez inclus d'autres caractères que des chiffres, veuillez recommencer svp");
 	                }    
 	        }
 	        utile.issueDeLaPartie(victoire, nbAleatoire);
--- Projet3/src/fr/plusoumoins/Utile.java
+++ Projet3/src/fr/plusoumoins/Utile.java
@@ -1,10 +1,12 @@
 package fr.plusoumoins;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Utile {
 
+	
+	
+	
 	public int[] transformationIntEnArrayInt (int proposition){
 	
 		String essai = String.valueOf(proposition);
@@ -15,6 +17,9 @@ public class Utile {
 	}
 		return montableau;
 }
+	
+	
+	
 	
 	public void comparaisonPlusOuMoins(int nbAleatoire[], int proposition[], int grandeurDuNb ) 
 	{	
@@ -38,6 +43,11 @@ public class Utile {
 		System.out.println(str);
 	}
 	
+	
+	
+	
+	
+	
 	public void issueDeLaPartie (boolean victoire, int nbAleatoire) {
 		if (victoire == true) {
 			System.out.println("Félicitations, vous avez réussi");
@@ -46,4 +56,36 @@ public class Utile {
 			System.out.println("Vous avez échoué, la bonne réponse était" + nbAleatoire);
 		}
 	}
+
+
+
+
+
+	public boolean verificationNb(String proposition, int grandeurDuNb) {
+		boolean nombre = true;
+		int i = 0;
+		char[] propositionArray = proposition.toCharArray();
+		if (grandeurDuNb != proposition.length())
+		{
+			nombre = false;
+		}
+		else
+		{
+			while (i != grandeurDuNb && nombre == true)
+			{
+				int tmp = (int) propositionArray[i];
+				if (tmp < 48 && tmp > 57) 
+				{
+					nombre = false;
+				}
+				else
+				{
+					i++;
+				}
+			}
+		}
+				
+		return nombre;
+	}
+	
 }
